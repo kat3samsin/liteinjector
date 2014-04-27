054621a7609429419381d170f217dff9bb859e1f
katre <kat3samsin@gmail.com>
2014-04-28 06:44:00 +0800

    additional methods

--- liteinjector.js
+++ liteinjector.js
@@ -1,18 +1,24 @@
 var liteinjector = {
    dependencies: {},
 
-   addDepedency : function(qualifier, obj){
-      this.dependencies[qualifier] = obj; 
+   addDepedency: function(qualifier, obj, isSingleton) {
+      if (!isSingleton || !this.dependencies[qualifier]) {
+         this.dependencies[qualifier] = new obj();
+      }
    },
 
-   get : function(func){
+   hasDependency: function(qualifier) {
+      return this.dependencies[qualifier] ? true : false;
+   },
+
+   get: function(func) {
       var obj = new func;
       var dependencies = this.resolveDependencies(func);
       func.apply(obj, dependencies);
       return obj;
    },
 
-   resolveDependencies : function(func) {
+   resolveDependencies: function(func) {
       var args = this.getArguments(func);
       var dependencies = [];
       for ( var i = 0; i < args.length; i++) {
@@ -21,10 +27,14 @@ var liteinjector = {
       return dependencies;
    },
    
-   getArguments : function(func) {
+   getArguments: function(func) {
       //This regex is from require.js
       var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
       var args = func.toString().match(FN_ARGS)[1].split(',');
       return args;
+   },
+
+   dispose: function() {
+      this.dependencies = {};
    }
 };
