72dde565edf9d17e347da19df98dbf018663b3e0
katre <kat3samsin@gmail.com>
2014-05-01 20:13:38 +0800

    Added new methods - addObject - getObject
    Renamed addDependency > addClass

--- liteinjector.js
+++ liteinjector.js
@@ -1,40 +1,69 @@
-var liteinjector = {
+//A lightweight IoC container based on Martin Fowler's Inversion of Control Containers and the Dependency Injection pattern.
+var injector = {
    dependencies: {},
-
-   addDepedency: function(qualifier, obj, isSingleton) {
-      if (!isSingleton || !this.dependencies[qualifier]) {
-         this.dependencies[qualifier] = new obj();
-      }
+   addClass: function(qualifier, obj, isSingleton) {
+      this.dependencies[qualifier] = {
+         obj: obj,
+         isSingleton: isSingleton,
+         instance: null
+      };
    },
-
+   addObject: function(qualifier, obj) {
+      this.dependencies[qualifier] = {
+         obj: obj,
+         isSingleton: false
+      };
+   },
    hasDependency: function(qualifier) {
       return this.dependencies[qualifier] ? true : false;
    },
-
-   get: function(func) {
+   getObject: function (obj) {
+      for (var prop in obj) {
+         if (this.dependencies[prop]) {
+            obj[prop] = this.dependencies[prop].obj;
+         }
+      }
+      return obj;
+   },
+   getInstance: function(func){
       var obj = new func;
       var dependencies = this.resolveDependencies(func);
       func.apply(obj, dependencies);
       return obj;
    },
-
    resolveDependencies: function(func) {
       var args = this.getArguments(func);
       var dependencies = [];
-      for ( var i = 0; i < args.length; i++) {
-         dependencies.push(this.dependencies[args[i]]);
+      for (var idep = 0; idep < args.length; idep++) {
+         //Undefined dependency
+         if (!this.dependencies[args[idep]]) {
+            dependencies.push(null);
+            continue;
+         }
+
+         //Set object
+         if (this.dependencies[args[idep]].instance === undefined) {
+            dependencies.push(this.dependencies[args[idep]].obj);
+            continue;
+         }
+
+         //Instantiate class
+         var instance = this.dependencies[args[idep]].instance;
+         var isSingleton = this.dependencies[args[idep]].isSingleton;
+         if (isSingleton && instance) {
+            dependencies.push(instance);
+         } else if (isSingleton && !instance) {
+            instance = new this.dependencies[args[idep]].obj();
+         } else {
+            dependencies.push(new this.dependencies[args[idep]].obj());
+         }
       }
       return dependencies;
    },
-   
    getArguments: function(func) {
       //This regex is from require.js
       var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
       var args = func.toString().match(FN_ARGS)[1].split(',');
       return args;
-   },
-
-   dispose: function() {
-      this.dependencies = {};
    }
 };
